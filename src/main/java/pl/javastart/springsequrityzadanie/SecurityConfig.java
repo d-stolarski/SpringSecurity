57f342676d06c6e0379126db7627981f042b7b1f
Daniel Stolarski <d.stolarski@o2.pl>
2018-03-02 12:54:56 +0100

    initial commit

--- src/main/java/pl/javastart/springsequrityzadanie/SecurityConfig.java
+++ src/main/java/pl/javastart/springsequrityzadanie/SecurityConfig.java
@@ -2,6 +2,7 @@ package pl.javastart.springsequrityzadanie;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,6 +10,9 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
@@ -18,6 +22,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     }
 
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "select username,password, from user where username=?");
+    }
 
 }
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO USER(username, password) values ('admin', 'abcd1');
+INSERT INTO USER(username, password) values ('user', 'abcd2');
